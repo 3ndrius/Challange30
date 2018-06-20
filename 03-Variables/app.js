74f400361a06ccb8954aba555d7c4fca2fd928a2
3ndrius <andrzejo1991@gmail.com>
2018-06-20 16:40:38 +0200

    Finish v3

---
+++ 03-Variables/app.js
@@ -0,0 +1,19 @@
+
+const inputs = document.querySelectorAll('input');
+
+const proc= document.querySelector('.proc');
+
+function handleChange() {
+    // console.log(this.value);
+    proc.innerHTML=this.value;
+    const suffix = this.dataset.sizing || '';
+
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleChange));
+inputs.forEach(input => input.addEventListener("mousemove", handleChange));
+
+console.log(inputs);
