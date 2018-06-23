319f14d281dabc349b7fc201c443c204641f24e3
3ndrius <andrzejo1991@gmail.com>
2018-06-23 11:11:13 +0200

    Finish v5

--- 05-FlexGallery/app.js
+++ 05-FlexGallery/app.js
@@ -1,10 +1,20 @@
 const panels = document.querySelectorAll('.panel');
 
-function toogleOpen() {
-  console.log("hello");
+function toggleOpen() {
+
+    this.classList.toggle('open');
+}
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if(e.propertyName.includes('flex')){
+    this.classList.toggle('open-active');
+  }
+  
 }
 
 
-panels.forEach(panel => panel.addEventListener('click', toogleOpen));
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 
