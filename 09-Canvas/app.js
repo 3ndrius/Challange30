6b72c063a308f0d71dead1a4f16b58b7da971cbb
3ndrius <andrzejo1991@gmail.com>
2018-07-10 17:25:39 +0200

    Finish

--- 09-Canvas/app.js
+++ 09-Canvas/app.js
@@ -9,7 +9,42 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth= 10;
+let hue = 0;
 
 let isDrawing = false;
-lastX = 0;
-lastY = 0;
+let lastX = 0;
+let lastY = 0;
+let direction = true;
+
+function draw(e) {
+    if(!isDrawing) return ; //stop function
+    console.log(e);
+    ctx.strokeStyle =  `hsl(${hue}, 100%, 50%)`;
+    ctx.lineWidth = hue;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+    hue++;
+    
+   if(hue >= 360) {
+       hue = 0;
+   }
+   ctx.lineWidth++;
+
+}
+
+
+canvas.addEventListener('mousedown', (e) => {
+
+    isDrawing=true;
+    lastX = e.offsetX;
+    lastY = e.offsetY;
+
+});
+canvas.addEventListener('mouseup', () => isDrawing=false);
+canvas.addEventListener('mouseout', () => isDrawing=false);
+canvas.addEventListener('mousemove', draw);
