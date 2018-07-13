d0d2a195925487f684662a221ebd4e20b4cba552
3ndrius <andrzejo1991@gmail.com>
2018-07-13 16:55:25 +0200

    Finish

--- 11-Game/app.js
+++ 11-Game/app.js
@@ -1,27 +1,49 @@
-
 const boxes = document.querySelectorAll('.box');
 let array = Array.from(boxes);
 
+
 let flag = true;
 let count = 0;
+const who = document.querySelector('.who');
+
 
 function eventHandle(e) {
-    console.log(e);
-    
-    if(flag) {
+ 
+
+    if (flag) {
         this.classList.add('box-active');
+
         flag = false;
-        count++;
-    }
-    else{
+        z = "x";
+        array[e.target.id] = "x";
+        check(z);
+        this.removeEventListener('click', eventHandle, false);
+    } else {
         this.classList.add('box-active2');
-        flag=true;
-        count++;
+        flag = true;
+        array[e.target.id] = "o";
+        z = "o";
+        check(z);
     }
-    console.log(count);
-    // console.log(boxes);
-    console.log(array);
-  
+
+    function check(z) {
+
+        if ((array[0] === z && array[1] === z && array[2] === z) || (array[3] === z && array[4] === z && array[5] === z) || (array[6] === z && array[7] === z && array[8] === z) ||
+            (array[0] === z && array[4] === z && array[8] === z) || (array[2] === z && array[4] === z && array[6] === z) || (array[0] === z && array[3] === z && array[6] === z) || (array[1] === z && array[4] === z && array[7] === z) || (array[2] === z && array[5] === z && array[8] === z)) {
+
+            if (z === "x") {
+                console.log("win x");
+                who.innerHTML = "x";
+            } else {
+                console.log("win o");
+                who.innerHTML = "o";
+            }
+
+        }
+    }
+
+
 }
+
 
 array.forEach(box => box.addEventListener('click', eventHandle));
