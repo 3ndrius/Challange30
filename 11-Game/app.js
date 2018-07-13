b4f5c24d4175628727108711c86587674b63b776
3ndrius <andrzejo1991@gmail.com>
2018-07-13 15:13:33 +0200

    Adding cross and circle show

--- 11-Game/app.js
+++ 11-Game/app.js
@@ -1,9 +1,27 @@
 
 const boxes = document.querySelectorAll('.box');
+let array = Array.from(boxes);
+
+let flag = true;
+let count = 0;
 
 function eventHandle(e) {
     console.log(e);
     
+    if(flag) {
+        this.classList.add('box-active');
+        flag = false;
+        count++;
+    }
+    else{
+        this.classList.add('box-active2');
+        flag=true;
+        count++;
+    }
+    console.log(count);
+    // console.log(boxes);
+    console.log(array);
+  
 }
 
-boxes.forEach(box => box.addEventListener('click', eventHandle));
+array.forEach(box => box.addEventListener('click', eventHandle));
