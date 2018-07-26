1cba1c323d2c97c45a6a431d816c403c9f3024c5
3ndrius <andrzejo1991@gmail.com>
2018-07-26 15:40:33 +0200

    Adding progress bar v3

--- 12-VideoPlayer/app.js
+++ 12-VideoPlayer/app.js
@@ -5,11 +5,11 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress_filled');
+const progressBar = player.querySelector('.progress__filled');
 
 const toggle = player.querySelector('.toggle');
-const skipButton = player.querySelector('[data-skip]');
-const ranges = player.querySelectorAll('.plyer__slider');
+const skipButtons = player.querySelectorAll('[data-skip]');
+let ranges = player.querySelectorAll('.player__slider');
 
 
 
@@ -25,10 +25,48 @@ function updateButton() {
     console.log(icon);
 
 }
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.value);
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
+video.addEventListener('timeupdate', handleProgress);
+
 
 toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => {
+    if(mousedown ) {
+        scrub(e);
+    }
+});
+progress.addEventListener('mousedown', () => {
+    mousedown = true;
+});
+progress.addEventListener('mouseup', () => {
+    mousedown = false;
+});
