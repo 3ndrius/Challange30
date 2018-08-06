ea94e8d50b4a77c91989f62ef84f6322ff62f27a
3ndrius <andrzejo1991@gmail.com>
2018-08-06 17:44:20 +0200

    Adding cornify

--- 14-KeySequence/app.js
+++ 14-KeySequence/app.js
@@ -6,5 +6,14 @@ const secretCode = 'boss';
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
     pressed.push(e.key);
+   
+    pressed.splice(-secretCode.length-1, pressed.length -secretCode-length)
+
+    if(pressed.join('').includes(secretCode)){
+        console.log("You reach goal");
+        cornify_add();
+    }
+
+
     console.log(pressed);
 });
