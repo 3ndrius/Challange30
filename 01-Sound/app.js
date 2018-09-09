1db09822bd9e95b294287da2d086f08e7efbb88a
3ndrius <3ndrius@users.noreply.github.com>
2018-09-09 17:57:26 +0200

    Update app.js

--- 01-Sound/app.js
+++ 01-Sound/app.js
@@ -2,20 +2,13 @@ window.addEventListener("keydown", e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   const keys = document.querySelectorAll(".key");
-
-
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-
   key.classList.add("active");
-
-
   function removeTransition(e) {
     if (e.propertyName !== "transform") return;
     key.classList.remove("active");
   }
-
-
   keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 });
