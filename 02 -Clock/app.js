84f9f8c3e1507da58bd7656deccf78a87277bb17
3ndrius <3ndrius@users.noreply.github.com>
2018-09-10 20:10:32 +0200

    Update app.js

--- 02 -Clock/app.js
+++ 02 -Clock/app.js
@@ -2,30 +2,19 @@
 sHand = document.querySelector('.seconds-hand');
 mHand = document.querySelector('.minutes-hand');
 hHand = document.querySelector('.hours-hand');
-
-
 function setDate() {
-
-    const now = new Date();
-    const seconds = now.getSeconds(); 
+   const now = new Date();
+   const seconds = now.getSeconds(); 
    const audio = document.querySelector('audio');
-
    audio.play();
-
     secondHand= ((seconds / 60) * 360  + 90);
     sHand.style.transform = `rotate(${secondHand}deg)`;
-
     const minutes = now.getMinutes(); 
-
     minutesHand = ((minutes/ 60) *360 +90);
     mHand.style.transform = `rotate(${minutesHand}deg)`;
-
     const hours = now.getHours();
-
     hoursHand = ((hours/12) * 360 + 90);
     hHand.style.transform = `rotate(${hoursHand}deg)`;
-
 }
-
 setInterval(setDate, 1000);
 
