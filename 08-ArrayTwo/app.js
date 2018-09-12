c26172a89e65046888f26050d5b33f101aa66c35
3ndrius <3ndrius@users.noreply.github.com>
2018-09-12 20:23:46 +0200

    Update app.js

--- 08-ArrayTwo/app.js
+++ 08-ArrayTwo/app.js
@@ -1,11 +1,9 @@
-
     const people = [
       { name: 'Wes', year: 1988 },
       { name: 'Kait', year: 1986 },
       { name: 'Irv', year: 1970 },
       { name: 'Lux', year: 2015 },
     ];
-
     const comments = [
       { text: 'Love this!', id: 523423 },
       { text: 'Super good', id: 823423 },
@@ -13,28 +11,15 @@
       { text: 'Ramen is my fav food ever', id: 123523 },
       { text: 'Nice Nice Nice!', id: 542328 }
     ];
-
-    
-
     const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
-
     console.log({isAdult});
     // Array.prototype.every() // is everyone 19?
-
     const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
     console.log({allAdults});
-
-
     const comment = comments.find(comment => comment.id === 823423);
-
     console.log(comment);
-
-
     const index = comments.findIndex(comment => comment.id === 823423);
     console.log(index);
-
-   
-
     const newComments = [
       ...comments.slice(0, index),
       ...comments.slice(index + 1)
