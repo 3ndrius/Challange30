3093f71ba014f1d5adca7d3d2a6ec73b62e7f844
3ndrius <3ndrius@users.noreply.github.com>
2018-09-13 20:03:34 +0200

    Update app.js

--- 09-Canvas/app.js
+++ 09-Canvas/app.js
@@ -2,21 +2,17 @@
 
 const canvas = document.querySelector('#draw');
 const ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth= 10;
 let hue = 0;
-
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let direction = true;
-
 function draw(e) {
     if(!isDrawing) return ; //stop function
     console.log(e);
@@ -34,16 +30,12 @@ function draw(e) {
        hue = 0;
    }
    ctx.lineWidth++;
-
 }
-
-
 canvas.addEventListener('mousedown', (e) => {
 
     isDrawing=true;
     lastX = e.offsetX;
     lastY = e.offsetY;
-
 });
 canvas.addEventListener('mouseup', () => isDrawing=false);
 canvas.addEventListener('mouseout', () => isDrawing=false);
