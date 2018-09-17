f2f8bebdd3dff6f2ae440188d2679e5b60c9c334
3ndrius <andrzejo1991@gmail.com>
2018-09-17 17:45:10 +0200

    Add reup

--- 17-StickyNav/app.js
+++ 17-StickyNav/app.js
@@ -1,43 +1,20 @@
 const nav = document.querySelector('#main');
 
 const topOfNav = nav.offsetTop; // element. distance to the top of browser
-// window.scrollY how much you scroll down
-
-
-
+// window.scrollY how much you scroll down site
 
 function fixNav() {
     console.log(topOfNav);
     console.log(window.scrollY);
 
     window.scrollY >= topOfNav ? document.body.classList.add('fixed-nav') : document.body.classList.remove('fixed-nav');
-  
  
-
-  
 }
 
 window.addEventListener('scroll', fixNav);
 
 
 
-
-const social = document.querySelector('#social');
-const topOf = social.offsetTop;
-    
-    function fixNav() {
-      if(window.scrollY >= topOfNav ) {
-        document.body.classList.add('fixed-nav');
-        console.log("now register");
-      }
-      else{
-        document.body.classList.remove('fixed-nav');
-        console.log("now unregister");
-      }
-                          
-    }
-    
-    window.addEventListener('scroll', fixNav);
 
 
 
