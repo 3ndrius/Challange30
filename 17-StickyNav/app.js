f3685e316d83c441580cc172896887b0f7e8f863
3ndrius <andrzejo1991@gmail.com>
2018-09-18 16:11:42 +0200

    Finish 17

--- 17-StickyNav/app.js
+++ 17-StickyNav/app.js
@@ -6,6 +6,7 @@ const topOfNav = nav.offsetTop; // element. distance to the top of browser
 function fixNav() {
     console.log(topOfNav);
     console.log(window.scrollY);
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
 
     window.scrollY >= topOfNav ? document.body.classList.add('fixed-nav') : document.body.classList.remove('fixed-nav');
  
