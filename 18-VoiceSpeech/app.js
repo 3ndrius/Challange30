c2bcf4033cfcd79d7a53b44f8f7ad7762a7ee9f3
3ndrius <3ndrius@users.noreply.github.com>
2018-09-23 18:56:58 +0200

    Update app.js

--- 18-VoiceSpeech/app.js
+++ 18-VoiceSpeech/app.js
@@ -1,11 +1,7 @@
-
-
 //get a html
 var synth = window.speechSynthesis;
-
 const utterance = new SpeechSynthesisUtterance();
 let voices = [];
-
 const voiceSelect = document.querySelector('#voice-list');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const btnStart = document.querySelector('.btn-start');
@@ -14,16 +10,12 @@ utterance.text = document.querySelector('#area-text').value;
 const vol = document.querySelector('#vol');
 const spd = document.querySelector('#spd');
 const pth = document.querySelector('#pth');
-
-
 function populateVoices(){
     voices = this.getVoices();
     const voiceOption = voices.map(voice=> `<option value="${voice.name}"> ${voice.name} (${voice.lang}</option>`)
     .join('');
     voiceSelect.innerHTML = voiceOption;
 }
-
-
 function setVoice() {
     console.log(this.value);
     utterance.voice = voices.find(voice => voice.name === this.value);   
@@ -33,7 +25,6 @@ function toggle() {
     speechSynthesis.cancel();
     speechSynthesis.speak(utterance);
 }
-
 function setOption() {
     console.log(this.name, this.value);
     utterance[this.name] = this.value;
@@ -46,6 +37,4 @@ btnStart.addEventListener('click', toggle);
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
 voiceSelect.addEventListener('change', setVoice);
 options.forEach(option=> option.addEventListener('change', setOption));
-
-
 btnStop.addEventListener('click', () => toggle(false));
