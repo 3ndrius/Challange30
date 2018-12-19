c9734ec743c02f985be85d1de032b1201bf69652
3ndrius <andrzejo1991@gmail.com>
2018-12-19 15:29:30 +0100

    Finish examp #21
    Adding toggle sel/remSelect all and finish evnt del / local storage

--- 21-StorageEvDelegat/app.js
+++ 21-StorageEvDelegat/app.js
@@ -3,7 +3,8 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
-
+const btn = document.querySelector('.btn');
+const del = document.querySelector('.btn-del');
 
 function addItem(e){
     e.preventDefault();
@@ -29,5 +30,28 @@ function populateList(plates= [] , platesList){
         `;
     }).join('');
 }
+
+function toggleDone(e) {
+    if(!e.target.matches('input')) return;
+    console.log(e.target);
+    const el = e.target;
+    const index = el.dataset.index;
+    items[index].done = !items[index].done;
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+function toggleCheck(e) {
+    items.forEach(item => {
+        item.done = !item.done
+    })
+    localStorage.setItem('items', JSON.stringify(items));
+    populateList(items, itemsList);
+}
+
 addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+btn.addEventListener('click', toggleCheck);
+
+
+
 populateList(items, itemsList);
